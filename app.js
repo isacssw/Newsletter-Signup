e52f48f236087067ef88fe643b26f0412ce48b6c
Isac X <isacssw@gmail.com>
2021-03-03 11:58:50 +0000

    setting everything together

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require('request');
+const https = require("https");
+
 const app = express();
 
 app.use(express.static("public"));
@@ -15,10 +17,54 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname+"/signup.html");
 })
 
+//API key
+// 2c1eca6a67db1ba55d8e04b7a35072db-us1
+
+//List id
+// 9a9fa635c3
+
 app.post("/", function(req, res){
 
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
-  console.log(firstName, lastName, email);
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  app.post("/failure", function(req, res){
+    res.redirect("/");
+  })
+
+  const jsonData = JSON.stringify(data);
+  const url = "https://us1.api.mailchimp.com/3.0/lists/9a9fa635c3"
+  const options = {
+    method: "POST",
+    auth: "isac1:2c1eca6a67db1ba55d8e04b7a35072db-us1"
+  }
+
+  const request = https.request(url, options, function(response){
+    if (response.statusCode === 200) {
+      res.sendfile(__dirname+"/success.html");
+    } else {
+      res.sendFile(__dirname+"/failure.html");
+    }
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+
 })
