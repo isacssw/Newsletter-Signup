457adc5b4962680dc4e69e60b51f798cbdfd9680
Isac X <isacssw@gmail.com>
2021-03-03 12:42:41 +0000

    changing port to use heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server is running on port 3000");
 })
 
